a8ca2356032039715e4d4824887b38179dced111
jchaselubitz <jchaselubitz@gmail.com>
2019-02-28 13:11:17 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+
 let addToy = false
+//================Show Toys=============
+function showToy(toy) {
+  const toyDiv = document.createElement('div')
+  toyDiv.className = 'card'
+  toyDiv.innerHTML =
+  `<h2>Toy Name: ${toy.name}</h2>
+  <img class="toy-avatar" src="${toy.image}"/>
+  <p>Likes: ${toy.likes} </p>
+  <button class"like-btn">Like</button>
+  <button class"like-btn">Delete</button>`
+  toyDiv.addEventListener('click', (event) => {
+    if (event.target.innerText === 'Like'){
+      toy.likes = toy.likes + 1
+      updateToy(toy)
+    }
+  })
+  toyDiv.addEventListener('click', (event) => {
+    if (event.target.innerText === 'Delete'){
+      deleteToy(toy)
+    }
+  })
+  return toyDiv
+}
 
-// YOUR CODE HERE
+function listToys(toys) {
+  toyCollection.innerHTML = ''
+  for (let i = 0; toys.length > i; i++) {
+    toy = showToy(toys[i])
+    toyCollection.append(toy)
+  }
+}
 
+//================Create Toy=============
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
@@ -15,5 +47,17 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+toyForm.addEventListener('submit', (event) => {
+  if (event.target.className === "add-toy-form")
+  event.preventDefault()
+  toy = {name: event.target.name.value, image: event.target.image.value, likes: 0}
+  postToy(toy)
+})
+
+
+function init() {
+  getToys()
+}
+init()
 
 // OR HERE!
